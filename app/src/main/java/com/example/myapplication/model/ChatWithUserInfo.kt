a8007dfec520971b9cc32e0e39b3fc25dc3716b7
madod <Rawan.hmaid.jed.k2@tuwaiq.edu.sa>
2021-12-27 10:19:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/model/ChatWithUserInfo.kt
@@ -0,0 +1,12 @@
+package com.fredrikbogg.android_chat_app.data.model
+
+import com.example.myapplication.Chat
+import com.example.myapplication.UserInfo
+
+
+
+
+data class ChatWithUserInfo(
+    var mChat: Chat,
+    var mUserInfo: UserInfo
+)
---
+++ app/src/main/java/com/example/myapplication/ui/DefeultViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.ui
+
+import android.provider.ContactsContract
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class DefeultViewModel:ViewModel() {
+    protected val mSnackBarText:MutableLiveData<>
+}
